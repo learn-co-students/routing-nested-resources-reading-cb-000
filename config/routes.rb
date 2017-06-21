ae60588114e065760305e39400867d06cd661aff
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-06-21 23:41:28 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
-  resources :authors, only: [:show]
-
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :authors, only: [:show] do
+    resources :posts, only: [:show, :index]
+  end
+  resources :posts, except: [:destroy]
 
   root 'posts#index'
 end
