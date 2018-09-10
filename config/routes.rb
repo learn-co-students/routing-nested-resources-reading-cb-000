457736677dcb0fa3a7c7b602c820691af495abc3
Quinn Dizon <qdizon@gmail.com>
2018-09-10 14:43:49 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :authors, only: [:show]
+  resources :authors, only: [:show] do
+
+    resources :posts, only: [:show, :index]
+
+  end
+  # get 'authors/:id/posts' => 'authors#posts_index'
+  # get 'authors/:id/posts/:post_id' => 'authors#post'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
